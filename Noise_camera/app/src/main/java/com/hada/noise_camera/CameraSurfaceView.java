c68678f59f945037a1cd2ab96f39c4c8c0225cd7
bangjinhyuk <jinhyuk0103@gmail.com>
2021-05-20 22:50:29 +0900

    [UPDATE] autofocusing,noise filter

--- Noise_camera/app/src/main/java/com/hada/noise_camera/CameraSurfaceView.java
+++ Noise_camera/app/src/main/java/com/hada/noise_camera/CameraSurfaceView.java
@@ -1,17 +1,33 @@
 package com.hada.noise_camera;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
     SurfaceHolder holder;
+    ImageView imageView;
     Camera camera = null;
-
+    private int cameraID = 0;
     public CameraSurfaceView(Context context) {
         super(context);
 
@@ -31,13 +47,23 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        camera = Camera.open(1);
+        camera = Camera.open();
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraID, info);
+        if (info.canDisableShutterSound) {
+            camera.enableShutterSound(false);
+        }
         Camera.Parameters parameters = camera.getParameters();
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
         parameters.set("orientation", "portrait");
         camera.setDisplayOrientation(90);
         parameters.setRotation(90);
+        camera.setParameters(parameters);
+        Mat noise = new Mat(new Size(parameters.getPictureSize().width,parameters.getPictureSize().width), CvType.CV_16S);
+
         try {
             camera.setPreviewDisplay(holder);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -46,6 +72,7 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         camera.startPreview();
+
     }
 
     @Override
@@ -62,5 +89,14 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
         } else {
             return false;
         }
+    }
+
+
+    public void setCameraID(int cameraID) {
+        this.cameraID = cameraID;
+    }
+
+    public SurfaceHolder getHolders() {
+        return holder;
     }
 }
--- Noise_camera/app/src/main/java/com/hada/noise_camera/MainActivity.java
+++ Noise_camera/app/src/main/java/com/hada/noise_camera/MainActivity.java
@@ -3,11 +3,13 @@ package com.hada.noise_camera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.content.Intent;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,12 +18,24 @@ import android.widget.Toast;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.Size;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     CameraSurfaceView surfaceView;
     ImageView imageView;
+
+    private Mat matInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +51,19 @@ public class MainActivity extends AppCompatActivity {
                 .setDeniedMessage("거부하셨습니다.")
                 .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE)
                 .check();
-
         Button button = findViewById(R.id.bt_shutter);
+        Button camera_front_back = findViewById(R.id.camera_front_back);
+
+        camera_front_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                surfaceView.setCameraID(1);
+//                surfaceView.surfaceDestroyed(surfaceView.getHolders());
+//                surfaceView.surfaceCreated(surfaceView.getHolders());
+////                Intent intent = getIntent();
+////                startActivity(intent);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,14 +72,109 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+//            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_, this, mLoaderCallback);
+        } else {
+            surfaceView.capture(new Camera.PictureCallback(){
+                @Override
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    Log.d("start()", "11111111");
+                    Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    matInput = new Mat();
+                    Utils.bitmapToMat(bmp,matInput);
+                    Mat noise = new Mat(matInput.size(), matInput.type());
+                    Core.randn(noise,0.0, 30.0);
+                    Matrix matrix = new Matrix();
+                    matrix.preRotate(90, 0, 0);
+                    Core.rotate(noise,noise,Core.ROTATE_90_CLOCKWISE);
+                    Bitmap noisebmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
+                    Utils.matToBitmap(noise,noisebmp);
+                    imageView = findViewById(R.id.imageView);
+                    imageView.setImageBitmap(noisebmp);
+                    Log.d("start()", matInput.size()+"");
+                }
+            });
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+
+        }
+    };
+//    public void start(){
+//        surfaceView.capture(new Camera.PictureCallback(){
+//            @Override
+//            public void onPictureTaken(byte[] data, Camera camera) {
+//                Log.d("start()", "11111111");
+//                Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+//                matInput = new Mat();
+//                Utils.bitmapToMat(bmp,matInput);
+//                Mat noise = new Mat(matInput.size(), matInput.type());
+//                Core.randn(noise,0.0, 30.0);
+//                Matrix matrix = new Matrix();
+//                matrix.preRotate(90, 0, 0);
+//                Core.rotate(noise,noise,Core.ROTATE_90_CLOCKWISE);
+//                Bitmap noisebmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
+//                Utils.matToBitmap(noise,noisebmp);
+//                imageView = findViewById(R.id.imageView);
+//                imageView.setImageBitmap(noisebmp);
+//                Log.d("start()", matInput.size()+"");
+//
+//            }
+//        });
+//    }
+
     public void capture(){
         surfaceView.capture(new Camera.PictureCallback() {
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
                 BitmapFactory.Options options = new BitmapFactory.Options();
                 options.inSampleSize = 8;
-                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                imageView.setImageBitmap(bitmap);
+                Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                OpenCVLoader.initDebug();
+
+//                imageView.setImageBitmap(bmp);
+                matInput = new Mat();
+                Utils.bitmapToMat(bmp,matInput);
+
+
+                Mat noise = new Mat(matInput.size(), matInput.type());
+                Log.d("data.lengthSize", data.length+""+matInput.type());
+                Log.d("Bitmap.lengthSize", bmp.getWidth()+""+bmp.getHeight());
+                Log.d("matInputSize", matInput.size()+"");
+
+                MatOfDouble mean = new MatOfDouble ();
+                MatOfDouble dev = new MatOfDouble ();
+                Core.meanStdDev(matInput,mean,dev);
+
+//                Core.randn(noise,mean.get(0,0)[0], dev.get(0,0)[0]);
+                Core.randn(noise,0.0, 30.0);
+
+                Core.add(matInput, noise, matInput);
+
+                Utils.matToBitmap(matInput,bmp);
+                Core.rotate(matInput,matInput,Core.ROTATE_90_CLOCKWISE);
+                // 이미지 중심으로 90도 회전 Matrix
+                Matrix matrix = new Matrix();
+                matrix.preRotate(90, 0, 0);
+                // 이미지 회전
+                Bitmap mbmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
+                Utils.matToBitmap(matInput,mbmp);
+
+
+//                Core.rotate(noise,noise,Core.ROTATE_90_CLOCKWISE);
+//                Bitmap noisebmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
+//                Utils.matToBitmap(noise,noisebmp);
+//
+//
+//                imageView.setImageBitmap(noisebmp);
 
                 camera.startPreview();
             }
