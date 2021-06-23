104bf4051ff0288d934639de0785dbff75fc290a
KangSeonHo <ksk981214@naver.com>
2021-06-23 16:47:07 +0900

    [Update] gallery 완료
    갤러리 기능 추가 완료

--- Noise_camera/app/src/main/java/com/hada/noise_camera/CustomAdapter.java
+++ Noise_camera/app/src/main/java/com/hada/noise_camera/CustomAdapter.java
@@ -1,6 +1,8 @@
 package com.hada.noise_camera;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +11,14 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
-    private ArrayList<Integer> mBitmaps;
+    ArrayList<String> items = new ArrayList<String>();
+    Context mContext;
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
         protected ImageView iv;
@@ -23,8 +29,9 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         }
     }
 
-    public CustomAdapter(ArrayList<Integer> list) {
-        this.mBitmaps = list;
+    public CustomAdapter(Context context,ArrayList<String> list) {
+        this.items = list;
+        this.mContext = context;
     }
 
     @Override
@@ -38,12 +45,13 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
 
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder viewHolder, int position) {
-
-        viewHolder.iv.setImageResource(mBitmaps.get(position));
+        viewHolder.iv.setPadding(2,2,2,2);
+        viewHolder.iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        Glide.with(mContext).load(items.get(position)).into(viewHolder.iv);
     }
 
     @Override
-    public int getItemCount() {
-        return (null != mBitmaps ? mBitmaps.size() : 0);
+    public int getItemCount(){
+        return items.size();
     }
 }
--- Noise_camera/app/src/main/java/com/hada/noise_camera/GalleryView.java
+++ Noise_camera/app/src/main/java/com/hada/noise_camera/GalleryView.java
@@ -1,56 +1,68 @@
-package com.hada.noise_camera;
+    package com.hada.noise_camera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.widget.GridView;
 
+import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class GalleryView extends AppCompatActivity {
 
-    private ArrayList<Integer> mItems;
+    private ArrayList<String> urls = new ArrayList<>();
     private CustomAdapter mAdapter;
-    private int count = -1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_view);
 
+        getAllPhotos();
+
         RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         GridLayoutManager mGridLayoutManager = new GridLayoutManager(this,3);
+        RecyclerView.ItemDecoration dividerItemDecoration =
+                new DividerItemDecorator(ContextCompat.getDrawable(this,R.drawable.divider));
         mRecyclerView.setLayoutManager(mGridLayoutManager);
-
-        mItems = new ArrayList<>();
-
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-        mItems.add(R.drawable.shutterimg);
-
-
-        mAdapter = new CustomAdapter(mItems);
-        mRecyclerView.setAdapter(mAdapter);
-
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
-                mGridLayoutManager.getOrientation());
         mRecyclerView.addItemDecoration(dividerItemDecoration);
 
+
+        mAdapter = new CustomAdapter(this,urls);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
+    }
+
+    private void getAllPhotos() {
+        String[] projection = new String[]{
+                MediaStore.Images.ImageColumns._ID,
+                MediaStore.Images.ImageColumns.DATA,
+                MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME, //the album it in
+                MediaStore.Images.ImageColumns.DATE_TAKEN,
+                MediaStore.Images.ImageColumns.MIME_TYPE
+        };
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection,null,null,MediaStore.Images.ImageColumns.DATE_ADDED + " DESC");
+
+        if(cursor != null) {
+            while (cursor.moveToNext()) {
+                String url = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+
+                urls.add(url);
+            }
+            cursor.close();
+        }
     }
 }
